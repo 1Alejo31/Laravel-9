9c63028453acd755e1af192f40986fedc7f9eb8f
alejo Gonzalez <alejo.g31@hotmail.com>
2024-11-04 19:09:42 -0500

    Se realiza creacion de las prieras rutas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * Route::get    | Consultar
+ * Route::post   | Guardar
+ * Route::delete | Eliminar
+ * Route::put    | Actualizar
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta Home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    //Simulacion de consulta a db
+    return $slug;
+});
+
+Route::get('buscar', function (Request $resquest) {
+    return $resquest->all();
 });
