4c53b479698504ff30d181b78ca15a9b3090a35e
alejo Gonzalez <alejo.g31@hotmail.com>
2024-11-05 21:18:28 -0500

    Se realizan instalaciones para login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
 @extends('template')
 @section('content')
-<h1>Home</h1>
+    <h1>Home</h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
+
 
 /**
  * Route::get    | Consultar
@@ -12,20 +13,41 @@ use Illuminate\Http\Request;
  */
 
 /**
+
 Route::get('/', [PageController::class, 'home'])->name('home');
 
 Route::get('blog', [PageController::class, 'blog'])->name('blog');
 
-Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
-
+Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 Route::get('buscar', function (Request $resquest) {
     return $resquest->all();
 });
  */
-Route::controller(PageController::class)->group(function () {
+
+ Route::controller(PageController ::class)->group(function () {
     Route::get('/',             'home')->name('home');
 
     Route::get('blog',          'blog')->name('blog');
 
     Route::get('blog/{post:slug}',   'post')->name('post');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
