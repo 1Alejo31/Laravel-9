bc21466ee3e6914c55717e12fd180e168a8bf997
alejo Gonzalez <alejo.g31@hotmail.com>
2024-11-06 18:50:17 -0500

    Se realiza integracion de panel admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -49,5 +50,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::resource('posts', PostController::class)->except(['show'])->middleware(['auth']);
+
 
 require __DIR__.'/auth.php';
